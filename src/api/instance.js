e736866d25b078b9051e4e516bb92366675e6f8d
ponchik009 <lolioknru@mail.ru>
2022-05-23 00:30:44 +0800

    фиксы слияния

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,6 +1,5 @@
-import { Http } from "@mui/icons-material";
 import trueAxios from "axios";
 
 export const axios = trueAxios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3001/',
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:3001/",
 });
