95a6c35c9a24045f6c2efdbd5142391753362e76
katyan-averr <averkaty21@mail.ru>
2022-05-22 23:21:12 +0800

    первая попытка верстки

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { getSchedule } from "./api/api";
 import "./App.css";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import GroupIcon from '@mui/icons-material/Group';
+import SchoolIcon from '@mui/icons-material/School';
+import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
+import { useSpring } from "react-spring";
+import { animated } from "react-spring";
+import { Paper, Typography } from "@mui/material";
 
 function App() {
   const [monday, setMonday] = React.useState(new Date());
   const [nowDate, setNowDate] = React.useState(new Date());
-
   const [schedule, setSchedule] = React.useState([]);
+
+  const [value, setValue] = React.useState(0);
 
   React.useEffect(() => {
     const date = new Date();
@@ -19,9 +30,25 @@ function App() {
   }, [monday]);
 
   return (
-    <div className="App">
-      <div>Текущая дата: {nowDate.toISOString().split("T")[0]}</div>
-      <div>Начало недели: {monday.toISOString().split("T")[0]}</div>
+    <div>
+      {/* <div>Текущая дата: {nowDate.toISOString().split("T")[0]}</div>
+      <div>Начало недели: {monday.toISOString().split("T")[0]}</div> */}
+
+      <Typography mt={2} ml={62} variant="h1" gutterBottom component="div">Расписание</Typography>
+
+    <Box sx={{ width: '100%'}}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction style={{marginLeft:'50px', marginTop:'230px'}} label="Расписание по группам" icon={<GroupIcon sx={{ fontSize: 100 }}/> } />
+        <BottomNavigationAction style={{marginLeft:'100px', marginTop:'230px'}} label="Расписание по преподавателям" icon={<SchoolIcon sx={{ fontSize: 100 }}/>} />
+        <BottomNavigationAction style={{marginLeft:'150px', marginTop:'230px'}} label="Расписание по аудиториям" icon={<MeetingRoomIcon sx={{ fontSize: 100 }}/>} />
+      </BottomNavigation>
+    </Box>
     </div>
   );
 }
