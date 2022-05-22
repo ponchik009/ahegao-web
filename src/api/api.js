da6723b1144296279eb7907b99cbe83ec7492d7a
ponchik009 <lolioknru@mail.ru>
2022-05-22 19:45:39 +0800

    методы апи для получения расписания, простенький вывод расписания

--- src/api/api.js
+++ src/api/api.js
@@ -2,8 +2,26 @@ import axios from "axios";
 
 const url = "http://localhost:3001/";
 
-export const getSchedule = (weekBegining, group) => {
-  axios
-    .get(`${url}schedule?week_begining=${weekBegining}&groups_like=${group}`)
+export const getScheduleByGroup = (weekBegining, group) => {
+  return axios
+    .get(
+      `${url}schedule?week_begining=${weekBegining}&groups_like=${group}&_sort=day`
+    )
+    .then((response) => response.data);
+};
+
+export const getScheduleByTeacher = (weekBegining, teacher) => {
+  return axios
+    .get(
+      `${url}schedule?week_begining=${weekBegining}&teachers_like=${teacher}&_sort=day`
+    )
+    .then((response) => response.data);
+};
+
+export const getScheduleByAuditory = (weekBegining, auditory) => {
+  return axios
+    .get(
+      `${url}schedule?week_begining=${weekBegining}&auditories_like=${auditory}&_sort=day`
+    )
     .then((response) => response.data);
 };
