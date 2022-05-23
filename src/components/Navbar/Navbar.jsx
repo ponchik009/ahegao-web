f83fac31e092c85669787d74349b5c51fd5e6745
katyan-averr <averkaty21@mail.ru>
2022-05-23 16:17:51 +0800

    изменила немного навбар

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,31 +1,56 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./Navbar.css";
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import GroupIcon from '@mui/icons-material/Group';
+import SchoolIcon from '@mui/icons-material/School';
+import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
 
 const Navbar = ({ animated = false }) => {
   const navigate = useNavigate();
-
+  const [value, setValue] = React.useState(0);
   return (
-    <nav className="nav">
-      <div
+    // <nav className="nav">
+    //   <div
+    //     className={`nav_item group ${animated ? "anim_group" : ""}`}
+    //     onClick={() => navigate("/groups", { state: { animated: true } })}
+    //   >
+    //     Группы
+    //   </div>
+    //   <div
+    //     className="nav_item teacher"
+    //     onClick={() => navigate("/groups", { state: { animated: true } })}
+    //   >
+    //     Преподаватели
+    //   </div>
+    //   <div
+    //     className="nav_item auditory"
+    //     onClick={() => navigate("/groups", { state: { animated: true } })}
+    //   >
+    //     Аудитории
+    //   </div>
+    // </nav>
+    <Box sx={{ width: '100%'}} >
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction style={{ marginTop:'15px'}} label="Расписание по группам" icon={<GroupIcon fontSize="large"/>} 
+         className={`nav_item group ${animated ? "anim_group" : ""}`}
+         onClick={() => navigate("/groups", { state: { animated: true } })}/>
+        <BottomNavigationAction style={{ marginTop:'15px'}} label="Расписание по преподавателям" icon={<SchoolIcon fontSize="large"/>} 
         className={`nav_item group ${animated ? "anim_group" : ""}`}
-        onClick={() => navigate("/groups", { state: { animated: true } })}
-      >
-        Группы
-      </div>
-      <div
-        className="nav_item teacher"
-        onClick={() => navigate("/groups", { state: { animated: true } })}
-      >
-        Преподаватели
-      </div>
-      <div
-        className="nav_item auditory"
-        onClick={() => navigate("/groups", { state: { animated: true } })}
-      >
-        Аудитории
-      </div>
-    </nav>
+        onClick={() => navigate("/teachers", { state: { animated: true } })}/>
+        <BottomNavigationAction style={{ marginTop:'15px'}} label="Расписание по аудиториям" icon={<MeetingRoomIcon fontSize="large"/>} 
+        className={`nav_item group ${animated ? "anim_group" : ""}`}
+        onClick={() => navigate("/auditories", { state: { animated: true } })}/>
+      </BottomNavigation>
+    </Box>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import HomePage from "./pages/Home/HomePage";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <HomePage />
     </BrowserRouter>
   </React.StrictMode>
 );
